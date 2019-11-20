ea680df186b8915c12ee6b2d21e13f3b6d2924fb
Patil <rohit.r.patil@accenture.com>
2019-11-20 19:22:26 +0530

    Third Commit

--- mcd/flask_restful_app/app.py
+++ mcd/flask_restful_app/app.py
@@ -6,6 +6,7 @@ app.config["DEBUG"] = True
 
 @app.route('/home', methods=['GET'])
 def home():
-    return "Wel-Come to McDonalds India"
+    return "Wel-Come to McDonalds"
 
-app.run()
+
+app.run(host='0.0.0.0')
