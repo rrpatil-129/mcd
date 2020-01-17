038254e7776fe5fdca3e75c9bd14cc40b31c58ee
Patil <rohit.r.patil@accenture.com>
2020-01-17 17:14:19 +0530

    Added restful code

--- mcd/flask_restful_app/app.py
+++ mcd/flask_restful_app/app.py
@@ -1,23 +1,23 @@
-import json
+# import json
+#
+#
+# def lambda_handler(event, context):
+#     # TODO implement
+#     print("Inside lambda function")
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
+
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-def lambda_handler(event, context):
-    # TODO implement
-    print("Inside lambda function")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+@app.route('/home', methods=['GET'])
+def home():
+    return "Wel-Come to McDonalds Project"
 
-# import flask
-#
-# app = flask.Flask(__name__)
-# app.config["DEBUG"] = True
-#
-#
-# @app.route('/home', methods=['GET'])
-# def home():
-#     return "Wel-Come to McDonalds Project"
-#
-#
-# app.run(host='0.0.0.0')
+
+app.run(host='0.0.0.0')
